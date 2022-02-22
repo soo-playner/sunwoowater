53c32cdd86db923c5db4b3d2f4905dfa3271d8e5
soo-willsoft <soo@logcompany.co.kr>
2022-02-22 10:57:40 +0900

    ADD :: 보너스 직급수당 추가

--- adm/bonus/bonus.rank_bonus.php
+++ adm/bonus/bonus.rank_bonus.php
@@ -4,130 +4,120 @@ $sub_menu = "600299";
 include_once('./_common.php');
 
 // $debug = 1;
-$category = 'mining';
+$category = 'rank_bonus';
 include_once('./bonus_inc.php');
 
 auth_check($auth[$sub_menu], 'r');
 
-
-
-// 승급수당
-
-$avata = explode(',', $bonus_row['limited']);
-$bonus_token = explode(',', $bonus_row['layer']);
-
-$group_rule = ['S1+S2','S2+S3','S3+S4','S4+S5','S5'];
-$group_rule_num = ['1,2','2,3','3,4','4,5','5'];
-
-// 보름(15일) 단위 산출
-$day = date('d', $timestr);
-$lastday = date('t', $timestr);
-
-if($day > 14 && $day <= 24){
-    $half = '1/2';
-    $half_frdate    = date('Y-m-1', $timestr); // 매월 1 시작일자
-    $half_todate    = date('Y-m-15', $timestr); // 매월 15
-}else{
-    $half = '2/2';
-    $half_frdate    = date('Y-m-16', $timestr); // 매월 15
-    $half_todate    = date('Y-m-'.$lastday, $timestr); // 매월 말일
-}
-
-
-//보름간 마이닝 합계 
-$total_order_query = "SELECT truncate(sum(mining),6) AS hap FROM soodang_mining WHERE day BETWEEN '{$half_frdate}' AND '{$half_todate}' ";
-$total_order_reult = sql_fetch($total_order_query);
-$total_order = $total_order_reult['hap'];
-$grade_order = ($total_order * $bonus_rate);
+// 직급수당
+$bonus_layer = $_GET['exc_layer'];
+$bonus_rates = explode(',', $bonus_row['rate']);
 
 ob_start();
+$pre_extra_sql = "SELECT COUNT(NO) AS cnt, sum(bonus) AS total from soodang_extra WHERE COUNT =  {$bonus_layer} ";
+$pre_extra = sql_fetch($pre_extra_sql);
+$pre_extra_cnt = $pre_extra['cnt'];
+$pre_extra_total = $pre_extra['total'];
+
+$member_level = ['센터','지점','지사','본부'];
+$member_level_target = ['center','jijum','jisa','bonbu'];
 
 // 설정로그 
-echo "<br><strong> 현재일 : " . $bonus_day . " |  " . $half . "월 (지급산정기준) : <span class='red'>" . $half_frdate . "~" . $half_todate . "</span><br>";
-echo "<br>- 해당기간 마이닝 지급총량 : <span class='red'>" . point_number($total_order).' '.$minings[0] ." </span>";
-echo "<br>- 승급수당 마이닝 할당 : <span class='red'> (".$bonus_row['rate']."%)  " . point_number($grade_order).' '.$minings[0]." </span>";
-
-/* for ($i = 0; $i < count($bonus_layer); $i++) {
-    echo "<br>- <strong>S" . ($i+1) . "  : ";
-    echo "승급수당 : <span class='red'>" . Number_format($bonus_layer[$i]) . " % </span>";
-    echo "</strong>";
-} */
+echo "<br><strong> 지급일 : " . $bonus_day . " |  지급차수 : <span class='red'>" . $bonus_layer . "차</span><br>";
+echo "직급수당 산정건수 : <span class='red'>".$pre_extra_cnt." 건</span>";
+echo " | 직급총 수당금액 : <span class='red'>".shift_auto($pre_extra_total);
 echo "</span><br><br>";
+
+echo "직급별 지급총액 : ";
+for($i=0; $i < count($bonus_rates); $i++){
+    echo $member_level[$i]." : <span class='blue'>". shift_auto($pre_extra_total * ($bonus_rates[$i]*0.01))."</span>  |  ";
+}
+
+echo "<br><br>";
 echo "<div class='btn' onclick=bonus_url('".$category."')>돌아가기</div>";
 ?>
 
 <html>
 
 <body>
-    <header>상품승급시작</header>
+    <header>직급수당지급</header>
     <div>
 
         <?
-        for ($z = count($bonus_layer); $z > 0; $z--) {
-            rankup($z);
+        for ($z = count($bonus_rates); $z > 0; $z--) {
+            rankbonus($z);
         }
 
-        function rankup($val)
+        function rankbonus($val)
         {
-            global $g5, $debug, $bonus_day, $code;
-            global $bonus_rate,$bonus_layer, $grade_order,$group_rule,$group_rule_num,$minings,$mining_target,$mining_hash;
+            global $g5, $debug, $bonus_day, $code,$bonus_layer;
+            global $bonus_rates,$bonus_layer, $member_level,$pre_extra_total,$member_level_target;
 
-            $num = $val - 1;
-
-            $rank_member_sql = "SELECT * from `g5_member` WHERE grade = {$val} ";
-            $rank_member_result = sql_query($rank_member_sql);
-            $rank_member_cnt = sql_num_rows($rank_member_result);
-
-            $rank_group_sql = "SELECT count(*) as cnt from g5_member WHERE grade in ({$group_rule_num[$num]}) ";
-            $rank_group_cnt = sql_fetch($rank_group_sql)['cnt'];
-
-            $rank_bonus = $grade_order*$bonus_layer[$num];
-
+            $num = $val-1;
+            $bonus_rate = ($bonus_rates[$num]*0.01);
+            $rank = $member_level_target[$num];
+            $total = $pre_extra_total * $bonus_rate;
+            $exc_bonus_total = 0;
+            
             echo "<br><br><div class='block'>";
-            echo "S{$val} ";
-
-
-
-
-            echo " : ".$bonus_layer[$num]."% ";
-            echo " | Bonus ::".$rank_bonus.' '.$minings[0];
-            echo " | ".$group_rule[$num]." = 1 / ".$rank_group_cnt;
+            echo $member_level[$num];
+            echo " : ".$bonus_rates[$num]."%  | ";
+            echo shift_auto($total);
             echo "</div>";
+            
+            $rank_member_sql = "SELECT {$rank},sum(bonus) as benefit from soodang_extra WHERE COUNT = {$bonus_layer} AND {$rank} != ''  Group by {$rank} ";
+            $rank_member = sql_query($rank_member_sql);
 
-            if($rank_member_cnt > 0){
-                while($row = sql_fetch_array($rank_member_result)){
-                    $mb_id = $row['mb_id'];
-                    $benefit = point_number($rank_bonus/$rank_group_cnt);
+            if($rank_member){
+                while($row = sql_fetch_array($rank_member)){
+                    $mb_id = $row[$rank];
+                    $benefit = $row['benefit']*$bonus_rate;
 
-                    echo "<br><span class='title box'> ".$mb_id."  - 승급수당 : <span class='blue'>".point_number($benefit).' '.$minings[0]."</span></span>";
+                    echo "<br><span class='title box'> ".$mb_id."  - 직급수당 : <span class='blue'>".shift_auto($benefit)."</span></span>";
                     
+                    $member_log_sql = "SELECT mb_id,bonus from soodang_extra WHERE COUNT = {$bonus_layer} AND {$rank} = '{$mb_id}' ";
+                    $member_log = sql_query($member_log_sql);
+
+                    echo "<code>";
+                    while($mem = sql_fetch_array($member_log)){
+                        echo "<br>".$mem['mb_id']." | ".$mem['bonus']." * ".$bonus_rate." = ".shift_auto($mem['bonus']*$bonus_rate);
+                    }
+                    echo "</code>";
+
+                    $rec='Rank Bonus By '.$member_level[$num].' :: '.shift_auto($benefit);
+                    $rec_adm = shift_auto($row['benefit'])." * ".$bonus_rate." = ".shift_auto($benefit);
+                    
+                    $record_result = soodang_record($mb_id, $code, $benefit,$rec,$rec_adm,$bonus_day,$bonus_layer,1);
 
                     if($benefit > 0){
-                        $rec='Rank Bonus By promote to S'.$val.' :: '.$benefit.' '.$minings[0];
-                        $rec_adm = $rec;
+                        echo "<span class=blue> ▶▶ 직급 수당 지급 : ".shift_auto($benefit)."</span><br><br>";
+                        $balance_up = "update g5_member set mb_balance = mb_balance + {$benefit}  where mb_id = '".$mb_id."'";
 
-                        echo "<span class=blue> ▶▶ 마이닝 지급 : ".$benefit.' '.$minings[0]."</span><br>";
-
-                        $record_result = mining_record($mb_id, $code, $benefit,$bonus_rate,$minings[0], $rec, $rec_adm, $bonus_day);
-
-                        if($record_result){
-                            $balance_up = "update g5_member set {$mining_target} = {$mining_target} + {$benefit}  where mb_id = '{$mb_id}' ";
-
-                            // 디버그 로그
-                            if($debug){
-                                echo "<code>";
-                                print_R($balance_up);
-                                echo "</code>";
-                            }else{
-                                sql_query($balance_up);
-                            }
+                        // 디버그 로그
+                        if($debug){
+                            echo "<code>";
+                            print_R($balance_up);
+                            echo "</code>";
+                        }else{
+                            sql_query($balance_up);
                         }
+
+                        $exc_bonus_total += $benefit;
+                        
                     }else{
-                        echo "<span class=blue> ▶▶ 수당 지급 : ".$benefit.' '.$minings[0]."</span><br>";
+                        echo "<span class=red> ▶▶ 지급수당금액 없음 </span><br>";
                     }
+
+
                 }
             }else{
                 echo "<span class='red'>지급 대상 없음</span>";
+            }
+
+            if($total > $exc_bonus_total){
+                echo "<br><br>미지급금액 : <span class='red'>";
+                echo shift_auto($total - $exc_bonus_total);
+                echo "</span>";
             }
         } // rankup
         ?>
@@ -135,11 +125,10 @@ echo "<div class='btn' onclick=bonus_url('".$category."')>돌아가기</div>";
         <? include_once('./bonus_footer.php'); ?>
 
         <?
-        if ($debug) {
-        } else {
+        if($debug){}else{
             $html = ob_get_contents();
             //ob_end_flush();
-            $logfile = G5_PATH . '/data/log/' . $code . '/' . $code . '_' . $bonus_day . '.html';
+            $logfile = G5_PATH.'/data/log/'.$code.'/'.$code.'_'.$bonus_layer.'.html';
             fopen($logfile, "w");
             file_put_contents($logfile, ob_get_contents());
         }
