70204671fb9bdc0c3cc542137ea52937b2336c8f
johyoung2022 <johyoung@logcompany.kr>
2022-03-07 18:03:52 +0900

    test_server.php 제어 관련

--- t_server.php
+++
@@ -1,6 +0,0 @@
-
-<style>
-	.test_site{position:fixed;background:black;top:0;z-index:99999;width:100%;color:white;font-size:12px;text-align:center; }
-</style>
-
-<div class="test_site">ZETABYTE_LOCAL_SERVER</div>
